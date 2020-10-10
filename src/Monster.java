3978422e0167f60036d41e0549e06d21b29c3490
aalsuwai-csumb <aalsuwaidi@csump.edu>
2020-10-09 17:22:03 -0700

    Created Monster Assignment

---
+++ src/Monster.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public abstract class Monster {
+
+    Integer hp;
+    Integer xp = 10;
+    Integer maxHP;
+    HashMap<String, Integer> items;
+
+    public Monster(Integer maxHP, Integer xp, HashMap<String, Integer> items) {
+        this.maxHP = maxHP;
+        hp = this.maxHP;
+        this.xp = xp;
+        this.items = items;
+    }
+
+    public Integer getHp() {
+        return hp;
+    }
+
+    public void setHp(Integer hp) {
+        this.hp = hp;
+    }
+
+    public Integer getXp() {
+        return xp;
+    }
+
+    public void setXp(Integer xp) {
+        this.xp = xp;
+    }
+
+    public Integer getMaxHP() {
+        return maxHP;
+    }
+
+    public void setMaxHP(Integer maxHP) {
+        this.maxHP = maxHP;
+    }
+
+    public HashMap<String, Integer> getItems() {
+        return items;
+    }
+
+    public void setItems(HashMap<String, Integer> items) {
+        this.items = items;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Monster monster = (Monster) o;
+        return hp.equals(monster.hp) &&
+                xp.equals(monster.xp) &&
+                maxHP.equals(monster.maxHP) &&
+                items.equals(monster.items);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(hp, xp, maxHP, items);
+    }
+
+    @Override
+    public String toString() {
+        return "hp=" + hp +"/" + maxHP;
+    }
+}
+
+
